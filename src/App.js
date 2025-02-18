d58acdb7663961d1e411643b914d0899eed99a11
Dominic Bloomfield <nooboon14@gmail.com>
2025-02-18 17:53:48 -0500

    changed Browser Router to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import GraphicDesign from "./pages/GraphicDesign";
 import TraditionalArt from "./pages/TraditionalArt";
 import Drawings from "./pages/Drawings";
